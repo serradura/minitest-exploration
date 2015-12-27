6acf0e9db8e0c441956315e9c79c094776efe36b
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2015-12-27 19:49:55 -0200

    Create test calculator with shoulda context

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -1,4 +1,4 @@
-require 'minitest/autorun'
+require 'test_helper'
 require 'calculator'
 
 class TestCalculator < Minitest::Test
---
+++ test/test_calculator_with_shoulda_context.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'calculator'
+
+class TestCalculatorWithShoulda < Minitest::Test
+  context 'a calculator' do
+    setup do # notice the difference
+      @calc = Calculator.new
+    end
+
+    should 'add two numbers properly' do
+      assert_equal 4, @calc.add(2, 2)
+    end
+
+    should 'not add incorrectly' do
+      refute_equal 5, @calc.add(2, 2)
+    end
+
+    should 'subtract two numbers properly' do
+      assert_equal 0, @calc.subtract(2, 2)
+    end
+
+    should 'not subtract incorrectly' do
+      refute_equal -1, @calc.subtract(2, 2)
+    end
+  end
+end
