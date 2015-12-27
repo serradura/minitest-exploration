ffe5351f9460f15d50558539b768c5e3d9492df2
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2015-12-27 20:32:43 -0200

    Improve the suite test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,11 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'shoulda/context'
+
+# Use the option below to have the default output with colors
+# Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+
+Minitest::Reporters.use! [
+  Minitest::Reporters::SpecReporter.new,
+  Minitest::Reporters::MeanTimeReporter.new
+]
