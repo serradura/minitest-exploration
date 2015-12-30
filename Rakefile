ab137395050e3bcfbfaf63f9c476cf291a8e55b1
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2015-12-30 02:42:21 -0200

    Create a benchmark suite

--- Rakefile
+++ Rakefile
@@ -6,3 +6,10 @@ Rake::TestTask.new do |task|
   task.name = 'test'
   task.description = 'Run tests'
 end
+
+Rake::TestTask.new do |task|
+  task.libs << %w(test lib)
+  task.pattern = 'test/benchmark/**/*_benchmark.rb'
+  task.name = 'bench'
+  task.description = 'Run benchmarks'
+end
---
+++ test/benchmark/operations_method_call_benchmark.rb
@@ -0,0 +1,48 @@
+require 'benchmark'
+
+require "calculator/basic_operations"
+
+class OperationsMethodCallBenchmark
+  class Base
+    def operations
+      Calculator::BasicOperations.instance
+    end
+  end
+
+  class DefinedMethod < Base
+    define_method(:add) do |num1, num2|
+      operations.public_send(__callee__, num1, num2)
+    end
+  end
+
+  class DynamicMethod < Base
+    def method_missing(method, *args)
+      operations.public_send(method, *args)
+    end
+  end
+
+  def self.setup
+    @dynamic = DynamicMethod.new
+    @defined = DefinedMethod.new
+    @numbers = (1..10_000)
+  end
+
+  def self.reports
+    Benchmark.bm do |x|
+      x.report("defined operation") do
+        @numbers.each { |n| @defined.add(n, n) }
+      end
+
+      x.report("dynamic operation") do
+        @numbers.each { |n| @dynamic.add(n, n) }
+      end
+    end
+  end
+
+  def self.run
+    setup
+    reports
+  end
+end
+
+OperationsMethodCallBenchmark.run
