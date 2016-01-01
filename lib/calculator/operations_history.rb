7cd14408e48ac1907c5540c4b39c38f55abd3971
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-01-01 13:20:48 -0200

    OperationsHistory can receives an observer

--- lib/calculator/operations_history.rb
+++ lib/calculator/operations_history.rb
@@ -1,33 +1,50 @@
 require 'forwardable'
+require 'observer'
 
 module Calculator
   class OperationsHistory
     extend Forwardable
+    include Observable
 
     def_delegator :@memo, :last
 
-    def initialize
+    def initialize(observer=nil)
       @memo = Array.new
+
+      apply_observer(observer)
     end
 
     def save(*args)
-      validate_operation!(args.first)
-      validate_numbers!(args.slice(1..4))
+      validate! args
 
-      @memo << build_entry(*args)
+      changed
+      @memo << build_record(*args)
+
+      notify_observers(@memo)
     end
 
     private
+
+    def apply_observer(observer)
+      if observer.respond_to? :update
+        self.add_observer(observer)
+      end
+    end
+
+    def validate!(args)
+      validate_operation! args.first
+      validate_numbers! args[1..4]
+    end
 
     def validate_operation!(operation)
       raise ArgumentError unless BasicOperations.valid?(operation)
     end
 
-    def validate_numbers!(args)
-      raise ArgumentError unless args.all? { |arg| arg.kind_of? Numeric }
+    def validate_numbers!(numbers)
+      raise ArgumentError unless numbers.all? { |number| number.kind_of? Numeric }
     end
 
-    def build_entry(operation, num1, num2, result)
+    def build_record(operation, num1, num2, result)
       {
         operation: operation,
         num1: num1,
